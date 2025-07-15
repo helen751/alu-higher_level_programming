785bdb2511eadcacbc25af12f9823fd67aaefac8
Helen751 <okerekehelenugoeze@gmail.com>
2025-07-15 18:00:01 +0200

    u

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,8 @@
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number of chars."""
+    """
+    Writes a string to a text file (UTF8) and returns the number of chars.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
