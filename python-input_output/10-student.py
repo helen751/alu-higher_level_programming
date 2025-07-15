3510bf17363e1046ac1875d27c3abbdabd048c59
Helen751 <okerekehelenugoeze@gmail.com>
2025-07-15 17:54:29 +0200

    u

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""
-Defines a Student class with filter for to_json.
+"""Module that defines a Student class with filtered serialization.
 """
 
 
 class Student:
-    """
-    Student class.
+    """Defines a student by first_name, last_name, and age.
     """
     def __init__(self, first_name, last_name, age):
+        """Instantiate with first_name, last_name, and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """Returns a dict representation of the Student.
+        If attrs is a list of strings, only attributes named in this list
+        will be returned.
         """
-        Returns dictionary representation of a Student instance.
-        If attrs is a list of strings, only attributes in this list are returned.
-        """
-        if isinstance(attrs, list) and all(type(attr) == str for attr in attrs):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {k: getattr(self, k)
+                    for k in attrs if hasattr(self, k)}
         return self.__dict__
