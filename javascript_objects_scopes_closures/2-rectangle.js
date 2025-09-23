eec0abb342ebc89ece57b7afb37fec5bf92c7f27
Helen751 <okerekehelenugoeze@gmail.com>
2025-09-23 19:57:38 +0200

    u

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {}; // return an empty object if invalid
+      // Instead of returning an empty object, just not setting width/height
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
 module.exports = Rectangle;
