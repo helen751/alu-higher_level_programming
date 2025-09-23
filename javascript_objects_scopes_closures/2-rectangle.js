8e677b51532654908fbcd174828498db5f9e2254
Helen751 <okerekehelenugoeze@gmail.com>
2025-09-23 19:45:36 +0200

    u

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-      return {};
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
     }
-    this.width = w;
-    this.height = h;
   }
 }
+
 module.exports = Rectangle;
